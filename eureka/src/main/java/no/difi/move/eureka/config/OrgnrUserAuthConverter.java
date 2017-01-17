83aca5dd794aaa470a0b2fcb00e54e1de21a178b
Nikolai Luthman <nikolai.luthman@gmail.com>
2017-01-17 11:57:18 +0100

    Eureka oauth2

---
+++ eureka/src/main/java/no/difi/move/eureka/config/OrgnrUserAuthConverter.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.difi.move.eureka.config;
+
+/**
+ *
+ * @author Nikolai Luthman <nikolai dot luthman at inmeta dot no>
+ */
+class OrgnrUserAuthConverter {
+
+}
