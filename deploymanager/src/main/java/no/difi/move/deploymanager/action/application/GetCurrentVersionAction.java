7810c648afd4cbf9841b485c413856d866d808b8
Johannes Molland <jom@difi.no>
2018-01-10 15:05:00 +0100

    Collapses catch clauses.

--- deploymanager/src/main/java/no/difi/move/deploymanager/action/application/GetCurrentVersionAction.java
+++ deploymanager/src/main/java/no/difi/move/deploymanager/action/application/GetCurrentVersionAction.java
@@ -1,8 +1,5 @@
 package no.difi.move.deploymanager.action.application;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
 import lombok.extern.slf4j.Slf4j;
 import no.difi.move.deploymanager.DeployManagerMain;
 import no.difi.move.deploymanager.action.DeployActionException;
@@ -10,8 +7,10 @@ import no.difi.move.deploymanager.domain.application.Application;
 import no.difi.move.deploymanager.domain.application.ApplicationMetadata;
 import no.difi.move.deploymanager.repo.DeployDirectoryRepo;
 
+import java.io.IOException;
+import java.util.Properties;
+
 /**
- *
  * @author Nikolai Luthman <nikolai dot luthman at inmeta dot no>
  */
 @Slf4j
@@ -30,9 +29,6 @@ public class GetCurrentVersionAction extends AbstractApplicationAction {
         try {
             Properties metadata = directoryRepo.getMetadata();
             t.setCurrent(ApplicationMetadata.builder().version(metadata.getProperty("version", "none")).build());
-        } catch (FileNotFoundException ex) {
-            log.error(null, ex);
-            throw new DeployActionException("Failed to get current version", ex);
         } catch (IOException ex) {
             log.error(null, ex);
             throw new DeployActionException("Failed to get current version", ex);
