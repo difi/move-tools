213989db56f2c1e59b6071e9c24ea4f1fcff73fb
Johannes Molland <jom@difi.no>
2018-01-11 13:28:56 +0100

    Fixes log message.

--- deploymanager/src/main/java/no/difi/move/deploymanager/action/application/CheckHealthAction.java
+++ deploymanager/src/main/java/no/difi/move/deploymanager/action/application/CheckHealthAction.java
@@ -1,6 +1,7 @@
 package no.difi.move.deploymanager.action.application;
 
 import java.io.IOException;
+import java.net.ConnectException;
 import java.net.URL;
 import lombok.extern.slf4j.Slf4j;
 import no.difi.move.deploymanager.DeployManagerMain;
@@ -19,7 +20,7 @@ public class CheckHealthAction extends AbstractApplicationAction {
 
     @Override
     public Application apply(Application t) {
-        log.info("Shutdown running version.");
+        log.info("Performing health check.");
         try {
             Object content = new URL(getManager().getProperties().getProperty("healthURL")).getContent();
             t.setHealth(true);
