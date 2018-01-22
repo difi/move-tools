4d38d51973ab2af2d9e74535c794e9e2844db557
Johannes Molland <jom@difi.no>
2018-01-22 10:26:47 +0100

    WIP: Adds configuration validation.

--- deploymanager/src/main/java/no/difi/move/deploymanager/config/DeployManagerProperties.java
+++ deploymanager/src/main/java/no/difi/move/deploymanager/config/DeployManagerProperties.java
@@ -2,20 +2,32 @@ package no.difi.move.deploymanager.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotNull;
 import java.net.URL;
 
 @Data
+@Validated
 @ConfigurationProperties(prefix = "no.difi.move.deploymanager")
 public class DeployManagerProperties {
+    @NotNull
     private String root;
+    @NotNull
     private URL nexus;
+    @NotNull
     private String repository;
+    @NotNull
     private String groupId;
+    @NotNull
     private String artifactId;
+    @NotNull
     private URL shutdownURL;
+    @NotNull
     private URL healthURL;
+    @NotNull
     private URL nexusProxyURL;
-    private String environment;
+    private String environment = "";
+    @NotNull
     private boolean verbose;
 }
