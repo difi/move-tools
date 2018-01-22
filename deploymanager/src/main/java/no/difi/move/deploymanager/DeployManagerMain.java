b119f0fe1f9e53ac89edd089232814f3558ad9dd
Johannes Molland <jom@difi.no>
2018-01-22 12:41:48 +0100

    Adds simple custom configuration property validator.

--- deploymanager/src/main/java/no/difi/move/deploymanager/DeployManagerMain.java
+++ deploymanager/src/main/java/no/difi/move/deploymanager/DeployManagerMain.java
@@ -5,15 +5,17 @@ import lombok.extern.slf4j.Slf4j;
 import no.difi.move.deploymanager.command.AbstractCommand;
 import no.difi.move.deploymanager.config.CommandLineOptions;
 import no.difi.move.deploymanager.config.DeployManagerProperties;
+import no.difi.move.deploymanager.config.DeployManagerPropertiesValidator;
 import org.apache.commons.cli.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.validation.Validator;
 
 import java.util.List;
-import java.util.Properties;
 
 /**
  * @author Nikolai Luthman <nikolai dot luthman at inmeta dot no>
@@ -25,20 +27,32 @@ import java.util.Properties;
 @EnableConfigurationProperties(DeployManagerProperties.class)
 public class DeployManagerMain implements CommandLineRunner {
 
-    private Properties properties = new Properties();
+    private final DeployManagerProperties deployManagerProperties;
     private CommandLine commandLine;
     private Options options;
     private List<AbstractCommand> commands;
 
-    public DeployManagerMain(List<AbstractCommand> commands) {
+    public DeployManagerMain(List<AbstractCommand> commands, DeployManagerProperties managerProperties) {
         this.commands = commands;
+        this.deployManagerProperties = managerProperties;
     }
 
-    public static void main(String[] args) throws Throwable {
+    @Bean(name = "configurationPropertiesValidator")
+    public static Validator configurationPropertiesValidator() { // Required name and access modifier on custom Spring validator.
+        return new DeployManagerPropertiesValidator();
+    }
+
+    public static void main(String[] args) {
         SpringApplication.run(DeployManagerMain.class, args);
     }
 
-    private void parseCommandLine(String[] args) {
+    @Override
+    public void run(String... args) {
+        parseCommands(args);
+        runCommands();
+    }
+
+    private void parseCommands(String[] args) {
         CommandLineParser parser = new DefaultParser();
 
         try {
@@ -48,12 +62,6 @@ public class DeployManagerMain implements CommandLineRunner {
             log.error(null, ex);
             System.exit(1);
         }
-    }
-
-    @Override
-    public void run(String... args) {
-        parseCommandLine(args);
-        runCommands();
     }
 
     private void runCommands() {
