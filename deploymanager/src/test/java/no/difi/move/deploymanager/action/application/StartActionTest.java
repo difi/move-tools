c7080162e648119ef3b6fa5fa59542592ea3f6d1
Johannes Molland <jom@difi.no>
2018-02-06 10:34:27 +0100

    Adds StartAction test.

---
+++ deploymanager/src/test/java/no/difi/move/deploymanager/action/application/StartActionTest.java
@@ -0,0 +1,27 @@
+package no.difi.move.deploymanager.action.application;
+
+import no.difi.move.deploymanager.config.DeployManagerProperties;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class StartActionTest {
+
+    private StartAction target;
+
+    @Mock
+    private DeployManagerProperties propertiesMock;
+
+    @Before
+    public void setUp() {
+        target = new StartAction(propertiesMock);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void apply_toNull_shouldThrow() {
+        target.apply(null);
+    }
+}
