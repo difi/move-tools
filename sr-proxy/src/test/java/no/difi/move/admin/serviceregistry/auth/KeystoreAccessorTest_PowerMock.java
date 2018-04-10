122c1f1d570af50bf1583604046e6618db45ad55
Johannes Molland <jom@difi.no>
2018-04-10 14:17:12 +0200

    Adds SR proxy code (previously a move-admin component).

---
+++ sr-proxy/src/test/java/no/difi/move/admin/serviceregistry/auth/KeystoreAccessorTest_PowerMock.java
@@ -0,0 +1,81 @@
+package no.difi.move.admin.serviceregistry.auth;
+
+import no.difi.move.admin.serviceregistry.config.KeyStoreProperties;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.core.io.PathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.ByteArrayInputStream;
+import java.security.KeyStore;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({KeyStore.class, KeystoreAccessor.class, KeystoreProvider.class})
+public class KeystoreAccessorTest_PowerMock {
+
+    @Mock
+    KeyStore keyStore;
+
+    @Mock
+    Resource file;
+
+    @Before
+    public void before()throws Exception{
+        PowerMockito.mockStatic(KeyStore.class);
+        when(KeyStore.getInstance(anyString())).thenReturn(keyStore);
+
+        when(file.getInputStream()).thenReturn(new ByteArrayInputStream("test content".getBytes()));
+    }
+
+    @Test
+    public void testLoadKeyStoreWithoutPath()throws Exception{
+
+        KeyStoreProperties properties = new KeyStoreProperties();
+        properties.setAlias("alias");
+        properties.setPassword("password");
+        properties.setType("type");
+
+        KeystoreAccessor keystoreAccessor = new KeystoreAccessor(properties);
+
+        Assert.assertEquals(keyStore, keystoreAccessor.getKeyStore());
+    }
+
+    @Test
+    public void testLoadKeyStoreWithNONEPath()throws Exception{
+
+        KeyStoreProperties properties = new KeyStoreProperties();
+        properties.setAlias("alias");
+        properties.setPassword("password");
+        properties.setType("type");
+
+        properties.setPath(new PathResource("NONE"));
+
+        KeystoreAccessor keystoreAccessor = new KeystoreAccessor(properties);
+
+        Assert.assertEquals(keyStore, keystoreAccessor.getKeyStore());
+    }
+
+
+    @Test
+    public void testLoadKeyStoreWithPath() throws Exception{
+
+        KeyStoreProperties properties = new KeyStoreProperties();
+        properties.setAlias("alias");
+        properties.setPassword("password");
+        properties.setType("type");
+        properties.setPath(file);
+
+        KeystoreAccessor keystoreAccessor = new KeystoreAccessor(properties);
+
+        Assert.assertEquals(keyStore, keystoreAccessor.getKeyStore());
+    }
+}
---
+++ sr-proxy/src/test/java/no/difi/move/admin/serviceregistry/client/ServiceRegistryClientTest.java
@@ -0,0 +1,36 @@
+package no.difi.move.admin.serviceregistry.client;
+
+import com.nimbusds.jose.proc.BadJWSException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ServiceRegistryClientTest {
+
+    private ServiceRegistryClient target;
+
+    @Mock
+    private RestClient restClientMock;
+
+    @Before
+    public void setUp() {
+        target = new ServiceRegistryClient(restClientMock);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testConstructor_restClientArgumentIsNull_shouldThrow() {
+        target = new ServiceRegistryClient(null);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testLookupIdentifier_badJWSException_shouldThrow() throws BadJWSException {
+        when(restClientMock.getResource(anyString())).thenThrow(new BadJWSException("test exception"));
+        target.lookupIdentifier("organisasjonsnummer");
+    }
+}
